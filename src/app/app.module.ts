0d02ffc32f6faf1e4b4cea39acd1abe3feba8fb2
EduardoVichique18 <eduardogamer082@gmail.com>
2025-01-29 23:24:11 -0600

    feat: "Creacion componente catalogo"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
